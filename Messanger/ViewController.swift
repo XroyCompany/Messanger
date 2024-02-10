c8321224006ccdf5e5585ce7b7cde5b5ff59cdae
IbragimovRr <92021334+IbragimovRr@users.noreply.github.com>
2024-02-10 22:38:09 +0300

    Test

--- Messanger/ViewController.swift
+++ Messanger/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    //Eldar chert
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
